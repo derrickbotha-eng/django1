6caf99db211f43bdf36cb05116143239bbcf1bc1
botha <swahili1930@gmail.com>
2021-03-31 19:11:25 +0300

    something overstack is guiding me through

--- lazo/core/urls.py
+++ lazo/core/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     #page to show all topics
     url(r'^topics/$', views.topics, name="topics"),
 
+    #url for a single topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name="topic")
+
 
 ]
 
--- lazo/core/views.py
+++ lazo/core/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-from .models import Topic
+from .models import Topic, Entry
 
 # Create your views here.
 
@@ -17,4 +17,15 @@ def topics(request):
     context = {'topics':topics}
 
     return render(request, 'core/topics.html', context)
+
+
+
+def topic(request, topic_id):
+
+    #show an entry of a topic
+
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-dateTime')
+    context = {'topic':topic, 'entries': entries}
+    return render(request, 'core/topic.html', context)
 
