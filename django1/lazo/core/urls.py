be4c22bafab71e7762747a7a0a648d0c0a7eb125
botha <swahili1930@gmail.com>
2021-03-31 09:39:58 +0300

    linking the views for templates  and method for the database quering

---
+++ django1/lazo/core/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    #home page
+    url(r'^$', views.home, name='home'),
+
+    #page to show all topics
+    url(r'^topics&/', views.topics, name="topics"),
+
+
+]
+
+
